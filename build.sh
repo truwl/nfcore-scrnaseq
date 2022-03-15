792506a81a14332b4925b757f8c4ee817a87ef96
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:30:10 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-scrnaseq:1.1.0_0.1.0 -t truwl/nfcore-scrnaseq:latest .
+docker push truwl/nfcore-scrnaseq:1.1.0_0.1.0
+docker push truwl/nfcore-scrnaseq:latest
+        
